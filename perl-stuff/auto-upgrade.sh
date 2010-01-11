4cbc3c5044f587c147718dca8cb4e67871ffebc8
Christoph Maser <cmaser@gmx.de>
2010-01-11 20:53:49 +0000

    add optional argumetn for release

--- perl-stuff/auto-upgrade.sh
+++ perl-stuff/auto-upgrade.sh
@@ -12,14 +12,21 @@ USER="Christoph Maser <cmr@financial.com>"
 
 URL=$(grep "^Source: *" $1 | sed -e 's/^Source: //' -e "s/%{version}/${VERSION}/g" -e "s/%{name}/${NAME}/g")
 FILE=`basename $URL`
+
 wget -c $URL -O $SDIR/$FILE 
 
 grep real_version $1 && exit
 
-if [ $VERSION -ne $RPMVERSION ]
+if [ -n "$3" ];then
+ release="${3}"
+else
+ release="1"
+fi
+
+if [ $VERSION != $RPMVERSION ]
 then
 	sed  -i -e "s/^Version: .*/Version: $2/" \
-		 -e "s/^Release: .*/Release: 1%{?dist}/" \
+		 -e "s/^Release: .*/Release: ${release}%{?dist}/" \
 		 -e "/^%changelog/ a\* ${DATE} ${USER} - $VERSION-1\n- Updated to version ${VERSION}.\n" $1 
 fi	
 	
@@ -30,4 +37,4 @@ echo $URL
 rpmbuild -bs $1 
 
 egrep "# ExcludeDist.*el4" $1 || mock -r rpmforge-el4-x86_64 -v   --disable-plugin=root_cache /home/cmr/redhat/SRPMS/${NAME}-${VERSION}-1.src.rpm
-mock -r rpmforge-el5-x86_64 -v   --disable-plugin=root_cache /home/cmr/redhat/SRPMS/${NAME}-${VERSION}-1.src.rpm && svn diff
+mock -r rpmforge-el5-x86_64 -v   --disable-plugin=root_cache /home/cmr/redhat/SRPMS/${NAME}-${VERSION}-${release}.src.rpm && svn diff
